9cf16afbaea3f9417e3daac8d893514f20912164
Naboris <contact@naab.com>
2019-11-17 20:44:32 +0100

    change file structure

---
+++ lib/pages/add_player_page.dart
@@ -0,0 +1,124 @@
+import 'package:aym/pages/game_info_page.dart';
+import 'package:flutter/material.dart';
+
+class AddPlayerPage extends StatefulWidget {
+  AddPlayerPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _AddPlayerPageState createState() => _AddPlayerPageState();
+}
+
+class _AddPlayerPageState extends State<AddPlayerPage> {
+  List joueurs = <String>[];
+  int _counter = 0;
+
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Expanded(
+              // height: 200,
+              child: _listeJoueurs(),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: myController,
+                    ),
+                  ),
+                  FlatButton(
+                    color: Colors.purple,
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.all(2.0),
+                    splashColor: Colors.purpleAccent,
+                    onPressed: () {
+                      joueurs.add(myController.text);
+                    },
+                    child: Text(
+                      "+",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                ]),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+              Container(
+                child: FlatButton(
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(2.0),
+                  splashColor: Colors.purpleAccent,
+                  onPressed: () {
+                    _incrementCounter();
+                    _pushSaved();
+                  },
+                  child: Text(
+                    "Suivant",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              )
+            ])
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return GameInfoPage(joueurs: joueurs);
+        },
+      ),
+    );
+  }
+
+  void _incrementCounter() {
+    return setState(() {
+      _counter++;
+    });
+  }
+
+  Widget _listeJoueurs() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: joueurs.length,
+        itemBuilder: (context, i) {
+          return ListTile(
+              title: Text(
+            joueurs[i],
+          ));
+        });
+  }
+}
---
+++ lib/pages/countdown_pages.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class CountdownPage extends StatefulWidget {
+  CountdownPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<CountdownPage>
+    with TickerProviderStateMixin {
+  final myController = TextEditingController();
+  int temps = 1;
+  AnimationController controller;
+  String get timerString {
+    Duration duration = controller.duration * controller.value;
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(minutes: temps),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minuteur"),
+      ),
+      body: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    color: Colors.amber,
+                    height:
+                        controller.value * MediaQuery.of(context).size.height,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.center,
+                          child: AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Stack(
+                              children: <Widget>[
+//                                  Positioned.fill(
+//                                    child: CustomPaint(
+//                                        painter: CustomTimerPainter(
+//                                          animation: controller,
+//                                          backgroundColor: Colors.white,
+//                                          color: Colors.purple,
+//                                        )),
+//                                  ),
+                                Align(
+                                  alignment: FractionalOffset.center,
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      TextField(
+                                        decoration: InputDecoration(
+                                          border: OutlineInputBorder(),
+                                        ),
+                                        controller: myController,
+                                      ),
+                                      RaisedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            temps =
+                                                int.parse(myController.text);
+                                            controller = AnimationController(
+                                              vsync: this,
+                                              duration:
+                                                  Duration(minutes: temps),
+                                            );
+                                          });
+                                        },
+                                        child: Text("Je suis un bouton"),
+                                      ),
+                                      Text(
+                                        timerString,
+                                        style: TextStyle(
+                                            fontSize: 112.0,
+                                            color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      AnimatedBuilder(
+                          animation: controller,
+                          builder: (context, child) {
+                            return FloatingActionButton.extended(
+                                onPressed: () {
+                                  if (controller.isAnimating)
+                                    controller.stop();
+                                  else {
+                                    controller.reverse(
+                                        from: controller.value == 0.0
+                                            ? 1.0
+                                            : controller.value);
+                                  }
+                                },
+                                icon: Icon(controller.isAnimating
+                                    ? Icons.pause
+                                    : Icons.play_arrow),
+                                label: Text(
+                                    controller.isAnimating ? "Pause" : "Play"));
+                          }),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
